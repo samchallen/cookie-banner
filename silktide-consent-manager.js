ed700c12848e3c26eb56f95e643f8bf74bf16f53
samchallen <samchallen@gmail.com>
2025-08-03 20:36:32 +0100

    Update3 silktide-consent-manager.js

--- silktide-consent-manager.js
+++ silktide-consent-manager.js
@@ -79,7 +79,7 @@
       if (show) {
         this.hideCookieIcon();
         this.removeBanner();
-        document.body.style.overflow = "hidden"; // ONLY this line
+        document.body.style.overflow = "hidden"; // Only this line
         this.showBackdrop();
         this.updateCheckboxes(false);
       } else {
@@ -177,21 +177,18 @@
     }
 
     setupEventListeners() {
-      // Banner
       if (this.banner) {
         this.banner.querySelector(".accept-all")?.addEventListener("click", () => this.handleChoice(true));
         this.banner.querySelector(".reject-all")?.addEventListener("click", () => this.handleChoice(false));
         this.banner.querySelector(".preferences")?.addEventListener("click", () => this.toggleModal(true));
       }
 
-      // Modal
       if (this.modal) {
         this.modal.querySelector(".modal-close")?.addEventListener("click", () => this.toggleModal(false));
         this.modal.querySelector(".preferences-accept-all")?.addEventListener("click", () => this.handleChoice(true));
         this.modal.querySelector(".preferences-reject-all")?.addEventListener("click", () => this.handleChoice(false));
       }
 
-      // Cookie icon
       if (this.cookieIcon) {
         this.cookieIcon.addEventListener("click", () => this.toggleModal(true));
       }
@@ -244,11 +241,14 @@
     }
 
     getIconHTML() {
-      return `<svg width="38" height="38" viewBox="0 0 38 38"><circle cx="19" cy="19" r="18" fill="#000"/></svg>`;
+      return `
+        <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19.1172 1.15625C19.0547 0.734374 18.7344 0.390624 18.3125 0.328124C16.5859 0.0859365 14.8281 0.398437 13.2813 1.21875L7.5 4.30469C5.96094 5.125 4.71875 6.41406 3.95313 7.98437L1.08594 13.8906C0.320314 15.4609 0.0703136 17.2422 0.375001 18.9609L1.50781 25.4297C1.8125 27.1562 2.64844 28.7344 3.90625 29.9531L8.61719 34.5156C9.875 35.7344 11.4766 36.5156 13.2031 36.7578L19.6875 37.6719C21.4141 37.9141 23.1719 37.6016 24.7188 36.7812L30.5 33.6953C32.0391 32.875 33.2813 31.5859 34.0469 30.0078L36.9141 24.1094C37.6797 22.5391 37.9297 20.7578 37.625 19.0391C37.5547 18.625 37.2109 18.3125 36.7969 18.25C32.7734 17.6094 29.5469 14.5703 28.6328 10.6406C28.4922 10.0469 28.0078 9.59375 27.4063 9.5C23.1406 8.82031 19.7734 5.4375 19.1094 1.15625H19.1172Z" fill="#ffffff"/>
+        </svg>
+      `;
     }
   }
 
-  // Attach to window
   window.silktideCookieBannerManager = {
     updateCookieBannerConfig: function (config) {
       if (!document.body) {
